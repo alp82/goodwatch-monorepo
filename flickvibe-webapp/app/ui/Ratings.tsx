fa91d52bd8882c89838c84a299b7b69cb83fe366
Alper Ortac <alper@spirable.com>
2023-03-10 21:47:40 +0100

    better visibility if ratings are clickable or not

--- flickvibe-webapp/app/ui/Ratings.tsx
+++ flickvibe-webapp/app/ui/Ratings.tsx
@@ -27,47 +27,47 @@ export default function Ratings({ vibeRatings, imdbRatings, metacriticRatings, r
         </div>
       )}
       <div className="mt-2 mb-2 text-lg font-bold">Ratings</div>
-      <ul className="flex gap-4 flex-wrap">
+      <ul className="underline-offset-2 flex gap-4 flex-wrap">
         <dl className="w-20 p-3 rounded-lg bg-green-700 shadow overflow-hidden text-center">
           <dd className="mt-1 text-5xl font-semibold tracking-tight text-gray-100">{vibeRatings?.vibes || '--'}</dd>
           <dt className="mt-2 truncate text-lg font-medium text-gray-300">Vibes</dt>
         </dl>
 
         <a className="" href={imdbRatings?.url} target="_blank">
-          <dl className={`rounded-lg border-4 border-white/[.5] ${imdbRatings?.url && 'hover:border-white/[.9]'} w-24 p-3 bg-imdb shadow overflow-hidden text-center flex flex-col justify-center`}>
+          <dl className={`${imdbRatings?.url ? 'hover:border-white/[.9] active:border-white/[.9]' : 'opacity-60'} rounded-lg border-4 border-white/[.5] w-24 p-3 bg-imdb shadow overflow-hidden text-center flex flex-col justify-center`}>
             <img className="h-6 object-contain" src={imdbLogo} />
             <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{imdbRatings?.score || '--'}</dd>
-            <dt className="truncate text-xs font-medium text-gray-700">Score</dt>
+            <dt className={`truncate text-xs font-medium text-gray-700 ${imdbRatings?.url && 'underline'}`}>Score</dt>
           </dl>
         </a>
 
         <a className="" href={metacriticRatings?.url} target="_blank">
-          <dl className={`rounded-lg border-4 border-white/[.2] ${metacriticRatings?.url && 'hover:border-white/[.45]'} w-56 p-3 bg-metacritic shadow overflow-hidden text-center flex flex-col justify-center`}>
+          <dl className={`${metacriticRatings?.url ? 'hover:border-white/[.45] active:border-white/[.45]' : 'opacity-60'} rounded-lg border-4 border-white/[.2] w-56 p-3 bg-metacritic shadow overflow-hidden text-center flex flex-col justify-center`}>
             <img className="h-6 object-contain" src={metacriticLogo} />
             <div className="flex justify-center gap-8">
               <div>
                 <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-100">{metacriticRatings?.metaScore || '--'}</dd>
-                <dt className="truncate text-xs font-medium text-gray-300">Metascore</dt>
+                <dt className={`truncate text-xs font-medium text-gray-300 ${metacriticRatings?.url && 'underline'}`}>Metascore</dt>
               </div>
               <div>
                 <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-100">{metacriticRatings?.userScore || '--'}</dd>
-                <dt className="truncate text-xs font-medium text-gray-300">User Score</dt>
+                <dt className={`truncate text-xs font-medium text-gray-300 ${metacriticRatings?.url && 'underline'}`}>User Score</dt>
               </div>
             </div>
           </dl>
         </a>
 
         <a className="" href={rottenTomatoesRatings?.url} target="_blank">
-          <dl className={`rounded-lg border-4 border-white/[.5] ${rottenTomatoesRatings?.url && 'hover:border-white/[.9]'} w-56 p-3 bg-rotten shadow overflow-hidden text-center flex flex-col align-middle`}>
+          <dl className={`${rottenTomatoesRatings?.url ? 'hover:border-white/[.9] active:border-white/[.9]' : 'opacity-60'} rounded-lg border-4 border-white/[.5] w-56 p-3 bg-rotten shadow overflow-hidden text-center flex flex-col align-middle`}>
             <img className="h-6 object-contain" src={rottenLogo} />
             <div className="flex justify-center gap-8">
               <div>
                 <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-50">{rottenTomatoesRatings?.tomatometer || '--'}</dd>
-                <dt className="truncate text-xs font-medium text-gray-100">Tomatometer</dt>
+                <dt className={`truncate text-xs font-medium text-gray-100 ${metacriticRatings?.url && 'underline'}`}>Tomatometer</dt>
               </div>
               <div>
-                <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-50">{rottenTomatoesRatings?.audienceScore || '--'}</dd>
-                <dt className="truncate text-xs font-medium text-gray-100">Audience Score</dt>
+                <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-50 no-underline">{rottenTomatoesRatings?.audienceScore || '--'}</dd>
+                <dt className={`truncate text-xs font-medium text-gray-100 ${metacriticRatings?.url && 'underline'}`}>Audience Score</dt>
               </div>
             </div>
           </dl>
