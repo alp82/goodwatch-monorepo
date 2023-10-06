6c75d38334be9137402e78582ed5085a5ce35425
Alper Ortac <alper@spirable.com>
2023-10-06 18:58:09 +0200

    fix synced files

--- flickvibe-flows/windmill/f/db/mongodb.py
+++ flickvibe-flows/windmill/f/db/mongodb.py
@@ -4,12 +4,12 @@ import wmill
 
 def init_mongodb():
     print(f"Initializing mongodb...")
-    db_name = wmill.get_variable("u/Alp/MONGODB_DB")
-    db_host = wmill.get_variable("u/Alp/MONGODB_HOST")
-    db_port = int(wmill.get_variable("u/Alp/MONGODB_PORT"))
-    db_user = wmill.get_variable("u/Alp/MONGODB_USER")
-    db_pass = wmill.get_variable("u/Alp/MONGODB_PASS")
     try:
+        db_name = wmill.get_variable("u/Alp/MONGODB_DB")
+        db_host = wmill.get_variable("u/Alp/MONGODB_HOST")
+        db_port = int(wmill.get_variable("u/Alp/MONGODB_PORT"))
+        db_user = wmill.get_variable("u/Alp/MONGODB_USER")
+        db_pass = wmill.get_variable("u/Alp/MONGODB_PASS")
         connect(
             db=db_name,
             host=db_host,
