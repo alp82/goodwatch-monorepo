378385245f04a6bd963dadd51b0a40d98e79449c
Alper Ortac <alper@spirable.com>
2023-11-23 01:55:08 +0100

    fixed rating overlay for small screens

--- flickvibe-webapp/app/ui/RatingProgressOverlay.tsx
+++ flickvibe-webapp/app/ui/RatingProgressOverlay.tsx
@@ -19,17 +19,22 @@ export default function RatingProgressOverlay({ ratings }: RatingsProps) {
   const score = hasScore ? Math.floor(ratings.aggregated_overall_score_normalized_percent) : null
   const vibeColorIndex = hasScore ? Math.floor(ratings.aggregated_overall_score_normalized_percent / 10) * 10 : null
   const progressPosition = typeof score === "number" ? score : 50
-  const badgePosition = typeof score === "number" ? (score / 100) * 55 + 20 : 57
+  const badgePositionSmallScreen = typeof score === "number" ? (score / 100) * 90 + 5 : 50
+  const badgePositionNormalScreen = typeof score === "number" ? (score / 100) * 55 + 20 : 57
 
   return (
-    <div className="absolute w-full rounded-t-md bg-gray-800 h-2 md:h-4">
+    <div className="absolute -top-1 w-full rounded-t-md bg-gray-800 h-2 md:h-4">
       {hasScore && (
         <>
-          <div className={`${vibeColorIndex == null ? 'bg-gray-700' : `bg-vibe-${vibeColorIndex}`} absolute -top-1 h-4 md:h-6 px-2 rounded-md flex items-center justify-center gap-2 text-gray-100 text-sm md:text-lg`} style={{left: `${badgePosition}%`, transform: "translateX(-50%)"}}>
+          <div className={`hidden md:flex ${vibeColorIndex == null ? 'bg-gray-700' : `bg-vibe-${vibeColorIndex}`} absolute -top-1 h-4 md:h-6 px-2 rounded-md items-center justify-center gap-2 text-gray-100 text-sm md:text-lg`} style={{left: `${badgePositionNormalScreen}%`, transform: "translateX(-50%)"}}>
             <small>Score:</small>
             <strong className="-top-2">{score}</strong>
           </div>
-          <div className={`${vibeColorIndex == null ? 'bg-gray-700' : `bg-vibe-${vibeColorIndex}`} h-full rounded-t-md`} style={{width: `${progressPosition}%`}}></div>
+          <div className={`flex md:hidden ${vibeColorIndex == null ? 'bg-gray-700' : `bg-vibe-${vibeColorIndex}`} absolute -top-1 h-4 md:h-6 px-2 rounded-md items-center justify-center gap-2 text-gray-100 text-sm md:text-lg`} style={{left: `${badgePositionSmallScreen}%`, transform: "translateX(-50%)"}}>
+            <small>Score:</small>
+            <strong className="-top-2">{score}</strong>
+          </div>
+          <div className={`${vibeColorIndex == null ? 'bg-gray-700' : `bg-vibe-${vibeColorIndex}`} h-full rounded-tl-md`} style={{width: `${progressPosition}%`}}></div>
         </>
       )}
     </div>
