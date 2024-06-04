2a1691ed6c11e7b57313c205ef9b841dcc1fa64c
Alper Ortac <alportac@gmail.com>
2024-06-05 00:49:55 +0200

    try to avoid minification in production build

--- goodwatch-webapp/vite.config.js
+++ goodwatch-webapp/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
   plugins: [
     // remixDevTools(),
     remix({
+      serverMinify: false,
       // ignoredRouteFiles: ["**/.*"],
       // TODO remove
       // serverModuleFormat: "cjs",
