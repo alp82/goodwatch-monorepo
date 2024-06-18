7bd0f5a1e4073407786ba26fa817146a451c14ff
Alper Ortac <alportac@gmail.com>
2024-06-18 12:52:45 +0200

    fix cookie banner

--- goodwatch-webapp/app/ui/CookieConsent.tsx
+++ goodwatch-webapp/app/ui/CookieConsent.tsx
@@ -38,30 +38,32 @@ export default function CookieConsent() {
 
   return (
     <>
-      <div className="fixed z-[200] bottom-0 left-0 right-0 sm:left-4 sm:bottom-4 w-full sm:max-w-md transition-transform duration-70">
-        <div className="text-gray-200 bg-slate-800 rounded-md m-2">
-          <div className="grid gap-2">
-            <div className="border-b border-slate-600 h-14 flex items-center justify-between p-4">
-              <h1 className="text-lg font-medium">We use cookies</h1>
-              🍪
-            </div>
-            <div className="p-4">
-              <p className="text-sm font-normal">
-                We use cookies to ensure you get the best experience on our website. For more information on how we use cookies, please see our cookie policy.
-                <br />
-                <br />
-                <span className="text-xs">By clicking "<span className="font-medium opacity-80">Accept</span>", you agree to our use of cookies.</span>
-                <br />
-                <a href="/privacy" className="text-xs underline">Learn more.</a>
-              </p>
-            </div>
-            <div className="flex gap-2 p-4 py-5 border-t border-slate-600 bg-background/20">
-              <button onClick={handleAcceptCookies} className="w-full p-2 bg-indigo-800 hover:bg-indigo-700 ring-1 ring-inset ring-gray-600 focus:z-10 font-semibold">Accept</button>
-              <button onClick={handleDeclineCookies} className="w-full p-2 bg-gray-800 hover:bg-gray-700 ring-1 ring-inset ring-gray-600 focus:z-10">Decline</button>
+      {consentGiven === 'undecided' ? (
+        <div className="fixed z-[200] bottom-0 left-0 right-0 sm:left-4 sm:bottom-4 w-full sm:max-w-md transition-transform duration-70">
+          <div className="text-gray-200 bg-slate-800 rounded-md m-2">
+            <div className="grid gap-2">
+              <div className="border-b border-slate-600 h-14 flex items-center justify-between p-4">
+                <h1 className="text-lg font-medium">We use cookies</h1>
+                🍪
+              </div>
+              <div className="p-4">
+                <p className="text-sm font-normal">
+                  We use cookies to ensure you get the best experience on our website. For more information on how we use cookies, please see our cookie policy.
+                  <br />
+                  <br />
+                  <span className="text-xs">By clicking "<span className="font-medium opacity-80">Accept</span>", you agree to our use of cookies.</span>
+                  <br />
+                  <a href="/privacy" className="text-xs underline">Learn more.</a>
+                </p>
+              </div>
+              <div className="flex gap-2 p-4 py-5 border-t border-slate-600 bg-background/20">
+                <button onClick={handleAcceptCookies} className="w-full p-2 bg-indigo-800 hover:bg-indigo-700 ring-1 ring-inset ring-gray-600 focus:z-10 font-semibold">Accept</button>
+                <button onClick={handleDeclineCookies} className="w-full p-2 bg-gray-800 hover:bg-gray-700 ring-1 ring-inset ring-gray-600 focus:z-10">Decline</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : null}
     </>
   )
 }
