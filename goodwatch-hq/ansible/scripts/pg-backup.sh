a95ab55f4bb3f2dfc0e7e9e67ed58d72220c0665
Alper Ortac <alportac@gmail.com>
2024-09-11 01:54:31 +0200

    update logging

--- goodwatch-hq/ansible/scripts/pg-backup.sh
+++ goodwatch-hq/ansible/scripts/pg-backup.sh
@@ -7,6 +7,8 @@ PGUSER="postgres"
 DBNAME="goodwatch"
 RETENTION_DAYS=7
 
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Starting backup"
+
 # Create backup
 pg_dump -U $PGUSER $DBNAME | gzip > $BACKUP_DIR/${DBNAME}_$DATE.sql.gz
 
@@ -18,3 +20,5 @@ find $BACKUP_DIR -type f -name "${DBNAME}_*.sql.gz" -mtime +$RETENTION_DAYS -exe
 
 # Cleanup: Remove remote backups older than retention period (using SSH on the remote storage)
 ssh u417661-sub1@u417661.your-storagebox.de "find / -type f -name '${DBNAME}_*.sql.gz' -mtime +$RETENTION_DAYS -exec rm {} \;"
+
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Backup complete"
