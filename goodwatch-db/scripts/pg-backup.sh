c572719936d29cf1afe769aecb8898c1207d4d32
Alper Ortac <alportac@gmail.com>
2024-09-12 05:53:30 +0200

    fix backup script

--- goodwatch-db/scripts/pg-backup.sh
+++ goodwatch-db/scripts/pg-backup.sh
@@ -5,7 +5,8 @@ DATE=$(date +"%Y%m%d%H%M")
 BACKUP_DIR="/root/backup"
 PGUSER="postgres"
 DBNAME="goodwatch"
-RETENTION_DAYS=7
+RETENTION_DAYS_LOCAL=2
+RETENTION_DAYS_REMOTE=7
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Starting backup"
 
@@ -13,12 +14,12 @@ echo "[$(date +"%Y-%m-%d %H:%M:%S")] Starting backup"
 pg_dump -U $PGUSER $DBNAME | gzip > $BACKUP_DIR/${DBNAME}_$DATE.sql.gz
 
 # Transfer to Hetzner Storage Box
-scp $BACKUP_DIR/${DBNAME}_$DATE.sql.gz u417661-sub1@u417661.your-storagebox.de:/
+scp -p 23 $BACKUP_DIR/${DBNAME}_$DATE.sql.gz u417661-sub1@u417661.your-storagebox.de:/
 
 # Cleanup: Remove local backups older than retention period
 find $BACKUP_DIR -type f -name "${DBNAME}_*.sql.gz" -mtime +$RETENTION_DAYS -exec rm {} \;
 
 # Cleanup: Remove remote backups older than retention period (using SSH on the remote storage)
-ssh u417661-sub1@u417661.your-storagebox.de "find / -type f -name '${DBNAME}_*.sql.gz' -mtime +$RETENTION_DAYS -exec rm {} \;"
+ssh -p23 u417661-sub1@u417661.your-storagebox.de "find / -type f -name '${DBNAME}_*.sql.gz' -mtime +$RETENTION_DAYS -exec rm {} \;"
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Backup complete"
