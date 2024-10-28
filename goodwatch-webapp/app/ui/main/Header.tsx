adc6b44f46973f9539b8160ff6fceeadb778a7df
Alper Ortac <alportac@gmail.com>
2024-10-28 06:39:32 +0100

    fix header menu direct close

--- goodwatch-webapp/app/ui/main/Header.tsx
+++ goodwatch-webapp/app/ui/main/Header.tsx
@@ -1,7 +1,4 @@
 import {
-	Disclosure,
-	DisclosureButton,
-	DisclosurePanel,
 	Menu,
 	MenuButton,
 	MenuItem,
@@ -12,9 +9,6 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import React, { Fragment } from "react"
 import { useLocation } from "react-router"
 
-import Search from "~/ui/Search"
-import { useUser } from "~/utils/auth"
-
 import {
 	ArrowTopRightOnSquareIcon,
 	BookmarkIcon,
@@ -22,210 +16,272 @@ import {
 import { EyeIcon } from "@heroicons/react/24/solid"
 import { Link } from "@remix-run/react"
 import logo from "~/img/goodwatch-logo.png"
+import Search from "~/ui/Search"
 import { GoogleSignInButton } from "~/ui/auth/GoogleSignInButton"
 import { SignOutLink } from "~/ui/auth/SignOutLink"
 import { GlobalLoading } from "~/ui/nav/GlobalLoading"
+import { useUser } from "~/utils/auth"
 
 export default function Header() {
 	const location = useLocation()
 	const isPage = (pathname: string) => location.pathname.startsWith(pathname)
+	const isPageExact = (pathname: string) => location.pathname === pathname
 
 	const { user, loading } = useUser()
 
 	return (
 		<div className="fixed top-0 z-50 w-full bg-gray-900">
 			<GlobalLoading />
-			<Disclosure as="nav" className="bg-gray-950/35">
-				{({ open }) => (
-					<>
-						<div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
-							<div className="relative flex h-16 items-center justify-between">
-								<div className="flex lg:hidden">
-									{/* Mobile menu button */}
-									<DisclosureButton className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-										<span className="sr-only">Open main menu</span>
-										{open ? (
-											<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-										) : (
-											<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-										)}
-									</DisclosureButton>
-								</div>
-								<div className="flex items-center px-2 lg:px-0">
-									<div className="flex-shrink-0">
-										<Link to="/" prefetch="render">
-											<img
-												className="h-10 w-auto"
-												src={logo}
-												alt="GoodWatch Logo"
-											/>
-										</Link>
-									</div>
-									<Link to="/" prefetch="render">
-										<div className="brand-header hidden md:block ml-2 text-2xl text-gray-100">
-											GoodWatch
-										</div>
-									</Link>
-									<div className="hidden lg:ml-6 lg:block">
-										<div className="flex space-x-4">
-											<Link
-												className={`rounded-md px-3 py-2 text-md font-semibold ${isPage("/discover") ? "text-white bg-indigo-800" : "text-gray-300"} hover:bg-indigo-900 hover:text-white`}
-												to="/discover"
-												prefetch="render"
-											>
-												Discover
-											</Link>
-										</div>
-									</div>
-								</div>
-								<div className="flex flex-1 justify-center px-2 lg:ml-6 lg:justify-end">
-									<div className="max-w-lg lg:max-w-7xl">
-										<Search />
-									</div>
-								</div>
-								<div className="lg:ml-2">
-									<div className="flex items-center">
-										{loading ? (
-											<></>
-										) : user ? (
-											<>
-												{/* Profile dropdown */}
-												<Menu as="div" className="relative ml-2 flex-shrink-0">
-													<MenuButton className="flex rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-														<span className="sr-only">Open user menu</span>
-														<img
-															className="h-8 w-8 rounded-full"
-															src={user.user_metadata.avatar_url}
-															alt={user?.user_metadata.name}
-															title={user?.user_metadata.name}
-														/>
-													</MenuButton>
-													<Transition
-														as={Fragment}
-														enter="transition ease-out duration-100"
-														enterFrom="transform opacity-0 scale-95"
-														enterTo="transform opacity-100 scale-100"
-														leave="transition ease-in duration-75"
-														leaveFrom="transform opacity-100 scale-100"
-														leaveTo="transform opacity-0 scale-95"
-													>
-														<MenuItems
-															anchor="bottom"
-															className="absolute right-0 z-50 mt-2 w-64 origin-top-right rounded-md bg-gray-950 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+			<nav className="bg-gray-950/35">
+				<div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+					<div className="relative flex h-16 items-center justify-between">
+						{/* Mobile menu button */}
+						<div className="flex lg:hidden">
+							<Menu as="div" className="relative inline-block text-left">
+								{({ open }) => (
+									<>
+										<MenuButton className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+											<span className="sr-only">Open main menu</span>
+											{open ? (
+												<XMarkIcon
+													className="block h-6 w-6"
+													aria-hidden="true"
+												/>
+											) : (
+												<Bars3Icon
+													className="block h-6 w-6"
+													aria-hidden="true"
+												/>
+											)}
+										</MenuButton>
+										<Transition
+											as={Fragment}
+											enter="transition ease-out duration-100"
+											enterFrom="transform opacity-0 scale-95"
+											enterTo="transform opacity-100 scale-100"
+											leave="transition ease-in duration-75"
+											leaveFrom="transform opacity-100 scale-100"
+											leaveTo="transform opacity-0 scale-95"
+										>
+											<MenuItems className="absolute left-0 z-50 mt-2 w-64 origin-top-left rounded-md bg-gray-950 py-2 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+												<MenuItem>
+													{({ focus }) => (
+														<Link
+															to="/"
+															prefetch="render"
+															className={`block px-4 py-2 text-base font-medium ${
+																isPageExact("/")
+																	? "bg-indigo-900 text-white"
+																	: "text-gray-300"
+															} ${focus ? "bg-gray-700 text-white" : ""}`}
 														>
-															<MenuItem>
-																<Link
-																	className={`
-																	  flex gap-2 items-center px-4 py-2
-																		text-base text-amber-500 hover:bg-gray-700 hover:text-white
-																	`}
-																	to="/discover?type=all&watchedType=plan-to-watch&streamingPreset=mine"
-																	prefetch="viewport"
-																>
-																	<BookmarkIcon className="w-5 h-5" />
-																	<span>
-																		What I{" "}
-																		<span className="font-extrabold">
-																			Plan to Watch
-																		</span>
-																	</span>
-																</Link>
-															</MenuItem>
-															<MenuItem>
-																<Link
-																	className={`
-																	  flex gap-2 items-center px-4 py-2
-																		text-base text-green-500 hover:bg-gray-700 hover:text-white
-																	`}
-																	to="http://localhost:3003/discover?type=all&watchedType=watched"
-																	prefetch="viewport"
-																>
-																	<EyeIcon className="w-5 h-5" />
-																	<span>
-																		What I{" "}
-																		<span className="font-extrabold">
-																			Already Watched
-																		</span>
-																	</span>
-																</Link>
-															</MenuItem>
-															<MenuItem>
-																<SignOutLink active={false} />
-															</MenuItem>
-														</MenuItems>
-													</Transition>
-												</Menu>
-											</>
-										) : (
-											<GoogleSignInButton />
-										)}
-									</div>
-								</div>
-							</div>
+															Home
+														</Link>
+													)}
+												</MenuItem>
+												<MenuItem>
+													{({ focus }) => (
+														<Link
+															to="/discover"
+															prefetch="render"
+															className={`block px-4 py-2 text-base font-medium ${
+																isPage("/discover")
+																	? "bg-indigo-900 text-white"
+																	: "text-gray-300"
+															} ${focus ? "bg-gray-700 text-white" : ""}`}
+														>
+															Discover
+														</Link>
+													)}
+												</MenuItem>
+												<div className="border-t border-gray-500">
+													<MenuItem>
+														{({ focus }) => (
+															<Link
+																to="/about"
+																prefetch="viewport"
+																className={`block px-4 py-2 text-base font-medium ${
+																	isPage("/about")
+																		? "bg-indigo-900 text-white"
+																		: "text-gray-300"
+																} ${focus ? "bg-gray-700 text-white" : ""}`}
+															>
+																About
+															</Link>
+														)}
+													</MenuItem>
+													<MenuItem>
+														{({ focus }) => (
+															<Link
+																to="/disclaimer"
+																prefetch="viewport"
+																className={`block px-4 py-2 text-base font-medium ${
+																	isPage("/disclaimer")
+																		? "bg-indigo-900 text-white"
+																		: "text-gray-300"
+																} ${focus ? "bg-gray-700 text-white" : ""}`}
+															>
+																Disclaimer
+															</Link>
+														)}
+													</MenuItem>
+												</div>
+												<div className="border-t border-gray-500">
+													<MenuItem>
+														{({ focus }) => (
+															<a
+																href="https://dev.to/t/goodwatch"
+																className={`flex items-center px-4 py-2 text-base font-medium ${
+																	focus
+																		? "bg-gray-700 text-white"
+																		: "text-gray-300"
+																}`}
+															>
+																Blog
+																<ArrowTopRightOnSquareIcon className="ml-2 h-5 w-5" />
+															</a>
+														)}
+													</MenuItem>
+													<MenuItem>
+														{({ focus }) => (
+															<a
+																href="https://status.goodwatch.app/status/services"
+																className={`flex items-center px-4 py-2 text-base font-medium ${
+																	focus
+																		? "bg-gray-700 text-white"
+																		: "text-gray-300"
+																}`}
+															>
+																Status Page
+																<ArrowTopRightOnSquareIcon className="ml-2 h-5 w-5" />
+															</a>
+														)}
+													</MenuItem>
+												</div>
+											</MenuItems>
+										</Transition>
+									</>
+								)}
+							</Menu>
 						</div>
-
-						<Transition
-							enter="duration-200 ease-out"
-							enterFrom="opacity-0 -translate-y-6"
-							enterTo="opacity-100 translate-y-0"
-							leave="duration-300 ease-out"
-							leaveFrom="opacity-100 translate-y-0"
-							leaveTo="opacity-0 -translate-y-6"
-						>
-							<DisclosurePanel className="lg:hidden text-lg">
-								<div className="space-y-1 px-2 pt-2 pb-3">
+						{/* Logo and desktop links */}
+						<div className="flex items-center px-2 lg:px-0">
+							<div className="flex-shrink-0">
+								<Link to="/" prefetch="render">
+									<img
+										className="h-10 w-auto"
+										src={logo}
+										alt="GoodWatch Logo"
+									/>
+								</Link>
+							</div>
+							<Link to="/" prefetch="render">
+								<div className="brand-header hidden md:block ml-2 text-2xl text-gray-100">
+									GoodWatch
+								</div>
+							</Link>
+							<div className="hidden lg:ml-6 lg:block">
+								<div className="flex space-x-4">
 									<Link
-										className={`block rounded-md px-3 py-2 font-medium ${isPage("/") ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}`}
-										to="/"
-										prefetch="render"
-									>
-										Home
-									</Link>
-									<Link
-										className={`flex items-center rounded-md px-3 py-2 font-medium ${isPage("/discover") ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}`}
+										className={`rounded-md px-3 py-2 text-md font-semibold ${
+											isPage("/discover")
+												? "text-white bg-indigo-800"
+												: "text-gray-300"
+										} hover:bg-indigo-900 hover:text-white`}
 										to="/discover"
 										prefetch="render"
 									>
 										Discover
 									</Link>
 								</div>
-								<div className="border-t border-gray-500 space-y-1 px-2 pt-2 pb-3">
-									<Link
-										className={`flex items-center rounded-md px-3 py-2 font-medium ${isPage("/about") ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}`}
-										to="/about"
-										prefetch="viewport"
-									>
-										About
-									</Link>
-									<Link
-										className={`flex items-center rounded-md px-3 py-2 font-medium ${isPage("/disclaimer") ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}`}
-										to="/disclaimer"
-										prefetch="viewport"
-									>
-										Disclaimer
-									</Link>
-								</div>
-								<div className="border-t border-gray-500 space-y-1 px-2 pt-2 pb-3">
-									<a
-										href="https://dev.to/t/goodwatch"
-										className={`flex items-center rounded-md px-3 py-2 font-medium ${isPage("/blog") ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}`}
-									>
-										Blog
-										<ArrowTopRightOnSquareIcon className="ml-2 h-5 w-5" />
-									</a>
-									<a
-										href="https://status.goodwatch.app/status/services"
-										className={`flex items-center rounded-md px-3 py-2 font-medium ${isPage("/status") ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"}`}
-									>
-										Status Page
-										<ArrowTopRightOnSquareIcon className="ml-2 h-5 w-5" />
-									</a>
-								</div>
-							</DisclosurePanel>
-						</Transition>
-					</>
-				)}
-			</Disclosure>
+							</div>
+						</div>
+						{/* Search bar */}
+						<div className="flex flex-1 justify-center px-2 lg:ml-6 lg:justify-end">
+							<div className="max-w-lg lg:max-w-7xl">
+								<Search />
+							</div>
+						</div>
+						{/* Profile and Sign-In/Out */}
+						<div className="lg:ml-2">
+							<div className="flex items-center">
+								{loading ? null : user ? (
+									<Menu as="div" className="relative ml-2 flex-shrink-0">
+										<MenuButton className="flex rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+											<span className="sr-only">Open user menu</span>
+											<img
+												className="h-8 w-8 rounded-full"
+												src={user.user_metadata.avatar_url}
+												alt={user?.user_metadata.name}
+												title={user?.user_metadata.name}
+											/>
+										</MenuButton>
+										<Transition
+											as={Fragment}
+											enter="transition ease-out duration-100"
+											enterFrom="transform opacity-0 scale-95"
+											enterTo="transform opacity-100 scale-100"
+											leave="transition ease-in duration-75"
+											leaveFrom="transform opacity-100 scale-100"
+											leaveTo="transform opacity-0 scale-95"
+										>
+											<MenuItems className="absolute right-0 z-50 mt-2 w-64 origin-top-right rounded-md bg-gray-950 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+												<MenuItem>
+													{({ focus }) => (
+														<Link
+															to="/discover?type=all&watchedType=plan-to-watch&streamingPreset=mine"
+															prefetch="viewport"
+															className={`flex gap-2 items-center px-4 py-2 text-base font-medium ${
+																focus
+																	? "bg-gray-700 text-white"
+																	: "text-amber-500"
+															}`}
+														>
+															<BookmarkIcon className="w-5 h-5" />
+															<span>
+																What I{" "}
+																<span className="font-extrabold">
+																	Plan to Watch
+																</span>
+															</span>
+														</Link>
+													)}
+												</MenuItem>
+												<MenuItem>
+													{({ active }) => (
+														<Link
+															to="http://localhost:3003/discover?type=all&watchedType=watched"
+															prefetch="viewport"
+															className={`flex gap-2 items-center px-4 py-2 text-base font-medium ${
+																active
+																	? "bg-gray-700 text-white"
+																	: "text-green-500"
+															}`}
+														>
+															<EyeIcon className="w-5 h-5" />
+															<span>
+																What I{" "}
+																<span className="font-extrabold">
+																	Already Watched
+																</span>
+															</span>
+														</Link>
+													)}
+												</MenuItem>
+												<MenuItem>
+													<SignOutLink active={false} />
+												</MenuItem>
+											</MenuItems>
+										</Transition>
+									</Menu>
+								) : (
+									<GoogleSignInButton />
+								)}
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
 		</div>
 	)
 }
