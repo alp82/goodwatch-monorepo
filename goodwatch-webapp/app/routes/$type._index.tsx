581176492595f11ffbe33946daac2c72756f7e15
Alper Ortac <alportac@gmail.com>
2025-02-05 18:25:36 +0100

    better tagline

--- goodwatch-webapp/app/routes/$type._index.tsx
+++ goodwatch-webapp/app/routes/$type._index.tsx
@@ -50,11 +50,11 @@ export default function Movies() {
 	return (
 		<div className="max-w-7xl mx-auto p-4">
 			<h2 className="mb-8 text-4xl font-medium text-gray-100">
-				The best{" "}
+				Discover{" "}
 				<span className="font-extrabold accent">
 					{type === "movies" ? "Movies" : "TV Shows"}
-				</span>{" "}
-				to watch
+				</span>
+				, Your Way!
 			</h2>
 			<div className="mt-4 flex flex-col gap-10">
 				{Object.values(mainNavigation).map((navItem) => {
