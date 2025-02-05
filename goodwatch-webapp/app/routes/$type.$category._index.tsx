df079933a961ad786af527203fb7c8174eaedc05
Alper Ortac <alportac@gmail.com>
2025-02-05 17:54:57 +0100

    don't use swiper in category page

--- goodwatch-webapp/app/routes/$type.$category._index.tsx
+++ goodwatch-webapp/app/routes/$type.$category._index.tsx
@@ -130,7 +130,7 @@ export default function MoviesCategory_index() {
 			<Breadcrumbs path={path} />
 
 			<div className="max-w-7xl mx-auto p-4 flex flex-col gap-12">
-				<h2 className="text-4xl font-medium text-gray-100">
+				<h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-medium text-gray-100">
 					The best{" "}
 					<span className="font-extrabold accent">
 						{mainNavigation[category].label}
@@ -142,12 +142,12 @@ export default function MoviesCategory_index() {
 					<div key={pageResult.path} className="flex flex-col gap-4">
 						<Link
 							to={pageResult.path}
-							className="flex items-end gap-1 py-2 px-4 rounded-lg bg-gray-950/50 hover:bg-gray-950/70 transition-all group"
+							className="flex flex-col md:flex-row gap-1 md:gap-4 items-start md:items-end py-2 px-4 rounded-lg bg-gray-950/50 hover:bg-gray-950/70 transition-all group"
 						>
-							<span className="text-6xl font-extrabold opacity-60 text-gray-400 group-hover:opacity-80 group-hover:text-amber-400">
+							<span className="text-4xl md:text-6xl font-extrabold opacity-60 text-gray-400 group-hover:opacity-80 group-hover:text-amber-400">
 								{pageResult.label}
 							</span>
-							<span className="flex gap-1.5 ml-4 text-lg font-semibold text-indigo-500 group-hover:text-indigo-400 ">
+							<span className="flex gap-1.5 text-base font-semibold text-indigo-500 group-hover:text-indigo-400 ">
 								Show all
 								<ArrowRightCircleIcon className="w-5" />
 							</span>
--- goodwatch-webapp/app/ui/explore/MovieTvList.tsx
+++ goodwatch-webapp/app/ui/explore/MovieTvList.tsx
@@ -1,6 +1,4 @@
 import React from "react"
-import { Autoplay, EffectCoverflow, FreeMode } from "swiper/modules"
-import { Swiper, SwiperSlide } from "swiper/react"
 import { type GetDiscoverResult, useDiscover } from "~/routes/api.discover"
 import type { DiscoverParams } from "~/server/discover.server"
 import { MovieTvCard } from "~/ui/MovieTvCard"
@@ -21,60 +19,19 @@ export default function MovieTvList({
 	const results = discoverResults || discover.data || []
 
 	return (
-		<div className="flex gap-8 items-center">
-			<div className="w-full">
-				<Swiper
-					autoplay={{
-						delay: 10000,
-						pauseOnMouseEnter: true,
-					}}
-					breakpoints={{
-						320: {
-							slidesPerView: 2,
-						},
-						480: {
-							slidesPerView: 4,
-						},
-						640: {
-							slidesPerView: 6,
-						},
-						768: {
-							slidesPerView: 8,
-						},
-						1024: {
-							slidesPerView: 8,
-						},
-					}}
-					centeredSlides={true}
-					coverflowEffect={{
-						rotate: 10,
-						stretch: 0,
-						depth: 50,
-						modifier: 1,
-						slideShadows: false,
-					}}
-					effect="coverflow"
-					freeMode={{
-						enabled: true,
-					}}
-					grabCursor={true}
-					loop={true}
-					modules={[Autoplay, EffectCoverflow, FreeMode]}
-					slidesPerView={2}
+		<div className="grid grid-flow-col gap-2 auto-cols-[minmax(8rem,12rem)] overflow-hidden">
+			{results.slice(0, 8).map((details) => (
+				<div
+					key={details.tmdb_id}
+					className="transition-transform ease-in-out duration-200"
 				>
-					{results.map((details) => (
-						<SwiperSlide key={details.tmdb_id}>
-							<div className="w-32 xl:w-44 transition-transform ease-in-out duration-200">
-								<MovieTvCard
-									details={details}
-									mediaType={details.media_type}
-									prefetch={false}
-								/>
-							</div>
-						</SwiperSlide>
-					))}
-				</Swiper>
-			</div>
+					<MovieTvCard
+						details={details}
+						mediaType={details.media_type}
+						prefetch={false}
+					/>
+				</div>
+			))}
 		</div>
 	)
 }
